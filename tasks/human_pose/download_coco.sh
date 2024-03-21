a93864baad6802e3c0abf2668deb789383e90993
Daniel Hackbarth <da_ha@mailbox.org>
2024-03-21 14:58:28 +0000

    Expand download script

--- tasks/human_pose/download_coco.sh
+++ tasks/human_pose/download_coco.sh
@@ -1,3 +1,25 @@
+#! /bin/bash
+set -e
+echo "Creating new 'coco_datasets' directory"
+mkdir coco_datasets
+cd coco_datasets
+
+echo "Downloading train2017..."
+mkdir train2017
 wget http://images.cocodataset.org/zips/train2017.zip
+
+echo "Downloading val2017..."
+mkdir val2017
 wget http://images.cocodataset.org/zips/val2017.zip
+
+echo "Downloading annotations..."
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+
+echo "Unzipping train2017..."
+unzip -q train2017.zip
+
+echo "Unzipping val2017..."
+unzip -q val2017.zip
+
+echo "Unzipping annotations..."
+unzip -q annotations_trainval2017.zip
